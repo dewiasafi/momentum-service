76c2ccfcc20e7f6c97c4920506e4dcb853c52a8f
dewiasafi <dewiayu.sfitr1@gmail.com>
2026-09-14 16:28:19 +0700

    feat: add list activities json endpoint

--- main.go
+++ main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+type Activity struct {
+	ID          string `json:"id"`
+	Title       string `json:"title"`
+	IsCompleted bool   `json:"is_completed"`
+	Notes       string `json:"notes"`
+}
+
 func main() {
 
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/api/v1/activities", getActivitiesHandler)
+
 	port := ":8080"
 	log.Printf("Server sedang berjalan di http://localhost%s\n", port)
 	err := http.ListenAndServe(port, nil)
@@ -20,4 +30,23 @@ func main() {
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Momentum API is running")
+}
+
+func getActivitiesHandler(w http.ResponseWriter, r *http.Request) {
+	activities := []Activity{
+		{ID: "1", Title: "Belajar Go Fundamental", IsCompleted: true, Notes: "Install pakai go get ..."},
+		{ID: "2", Title: "Setup Routing REST API", IsCompleted: false},
+	}
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	if err := json.NewEncoder(w).Encode(activities); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
 }
